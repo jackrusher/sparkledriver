0ddad56841b5408d1b6923c14bcacae4835cfb44
Arne Brasseur <arne@arnebrasseur.net>
2017-08-16 15:24:31 +0200

    Add script to generate API docs
    This makes it easy to keep a gh-pages branch up to date with generated docs.

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.machinepublishers/jbrowserdriver "0.17.9"]]
+  :plugins [[lein-codox "0.10.3"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "1.7.25"]
                                   [http-kit "2.3.0-alpha2"]
                                   [compojure "1.6.0"]
                                   [hiccup "1.0.5"]]}}
+  :codox {:output-path "gh-pages"}
   :deploy-repositories [["releases" :clojars]])
