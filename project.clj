0aa16c17594d2572d0088e45f4458eda74825978
Jack Rusher <jack@rusher.com>
2018-08-23 11:33:57 +0200

    update deps, new version

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject sparkledriver "0.2.2"
+(defproject sparkledriver "0.2.3"
   :description "A clojure wrapper for jBrowserDriver, which is a Selenium-compatible wrapper around JFX embedded WebKit."
   :url "https://github.com/jackrusher/sparkledriver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.machinepublishers/jbrowserdriver "0.17.11"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.machinepublishers/jbrowserdriver "1.0.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "1.7.25"]
-                                  [http-kit "2.3.0-alpha2"]
-                                  [compojure "1.6.0"]
+                                  [http-kit "2.3.0"]
+                                  [compojure "1.6.1"]
                                   [hiccup "1.0.5"]]}
              :codox {:dependencies [[codox-theme-rdash "0.1.2"]]
                      :plugins [[lein-codox "0.10.3"]]
